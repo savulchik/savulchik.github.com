08562871ad12198fb48fcc0443ede75bb22496c6
Stanislav Savulchik <s.savulchik@gmail.com>
2012-09-02 21:43:21 +0700

    Первая реализация резюме

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source :rubygems
+
+gem 'compass', '~> 0.12.2'
+gem 'susy', '~> 1.0'
---
+++ config.rb
@@ -0,0 +1,11 @@
+require 'susy'
+
+http_path = ""
+css_dir = "stylesheets"
+sass_dir = "sass"
+images_dir = "images"
+javascripts_dir = "javascripts"
+
+output_style = :compact
+relative_assets = true
+line_comments = false
