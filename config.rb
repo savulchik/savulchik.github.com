4bf3fcaa1e0f2579b404aea26963577e0d3d9268
Stanislav Savulchik <s.savulchik@gmail.com>
2012-09-23 14:05:40 +0700

    Отредактирован compass конфиг

--- config.rb
+++ config.rb
@@ -1,11 +1,9 @@
 require 'susy'
 
-http_path = ""
 css_dir = "stylesheets"
 sass_dir = "sass"
 images_dir = "images"
-javascripts_dir = "javascripts"
 
-output_style = :expanded
+output_style = (environment == :production) ? :compressed : :expanded
 relative_assets = true
 line_comments = false
